d3c8e925f09086f7659d39749163ff88effdbdc1
Lim Rui An, Ryan <ryanlim1259@gmail.com>
2017-11-07 20:07:05 -0800

    Addition of State Managers Base Struct and Custom State Manager to be used as a reference template in creating your own custom state managers. Minor changes made to BaseEngine.h to include the CSM for testing and showcase purposes.

---
+++ BaseEngine/BaseStateManager.h
@@ -0,0 +1,37 @@
+/******************************************************************************
+filename    BaseStateManager.h
+author      Rui An Ryan Lim
+DP email    l.ruianryan@digipen.edu
+
+Created on 7 November 2017
+
+Brief Description:
+This file contains a BaseStateManager, a generic struct that is for other state 
+managers to "inherit" by linking their own custom functions to the function 
+pointers within this struct.
+
+******************************************************************************/
+
+/* Header Guards */
+#ifndef _BASE_STATE_MANAGER_H
+#define _BASE_STATE_MANAGER_H
+
+/* Public Struct & Functions */
+
+typedef struct BaseStateManager
+{
+	/* Public Parameters */
+	/* No Public Parameters For This Class Is Supposed To Be Generic */
+
+	/* Public Function Pointers*/
+	/* These function pointers will be generic such that we can make custom scene based state managers with different linked functions */
+	/* Initiallizes, requires an instance of itself */
+	void(*Initiallize)();
+	/* Updates, requires an instance of itself as well as delta time */
+	void(*Update)();
+	/* Exits, requires an instance, calls free if memory was allocated */
+	void(*Exit)();
+
+}BaseStateManager;
+
+#endif //_BASE_STATE_MANAGER_H
